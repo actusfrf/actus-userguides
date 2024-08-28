4a75639739f66b0a5cd94d434f1cd506ef1af033
mtoub <med.toub@gmail.com>
2024-08-28 01:27:19 +0100

    fix: corrected ACTUS Demo button

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -20,7 +20,7 @@ function HomepageHeader() {
           <Link
             className="button button--secondary button--lg"
             to="/docs/intro">
-            Actus Demos
+            Actus Documentation
           </Link>
         </div>
       </div>
