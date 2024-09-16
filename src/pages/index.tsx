8ffb83d63d202de040cc11928f2001e4c7d92848
mtoub <med.toub@gmail.com>
2024-09-16 16:40:34 +0100

    fix: capitalized "ACTUS Documentation" button

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -20,7 +20,7 @@ function HomepageHeader() {
           <Link
             className="button button--secondary button--lg"
             to="/docs/intro">
-            Actus Documentation
+            ACTUS Documentation
           </Link>
         </div>
       </div>
